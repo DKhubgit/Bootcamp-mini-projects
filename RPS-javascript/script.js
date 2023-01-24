59c9b42d24799dfcc3252f95cde6c32efaa7b273
DanielK <Danielkang13@gmail.com>
2023-01-24 10:45:32 -0800

    added logic to determine the winner of the RPS game, alerts the user of the results from their choice

--- RPS-javascript/script.js
+++ RPS-javascript/script.js
@@ -6,6 +6,31 @@ document.getElementById("button").addEventListener("click", function(event) {
 })
 
 function startGame() {
-    let userChoice = prompt("Rock, Paper, or Scissors? \n R = Rock | P = Paper | S = Scissors")
-    alert(userChoice);
+    let userChoice = prompt("Rock, Paper, or Scissors? \n r = Rock | p = Paper | s = Scissors")
+    //R = 0, P = 1, S = 2
+    let array = ['r', 'p', 's'];
+    let compChoice = array[Math.floor(Math.random() * 3)];
+
+    if (userChoice == 'r' && compChoice == 'p') {
+        alert(`You-${userChoice}, Computer-${compChoice} \n Computer Wins!`)
+    } else if (userChoice == 'p' && compChoice == 's') {
+        alert(`You-${userChoice}, Computer-${compChoice} \n Computer Wins!`)
+    } else if (userChoice == 's' && compChoice == 'r') {
+        alert(`You-${userChoice}, Computer-${compChoice} \n Computer Wins!`)
+    } else if (userChoice == compChoice) {
+        alert(`You-${userChoice}, Computer-${compChoice} \n Draw!`)
+    } else if (userChoice == null) {
+        alert ("Thanks for Playing!")
+        return;
+    } else {
+        alert(`You-${userChoice}, Computer-${compChoice} \n YOU Win!`)
+    }
+
+    if (confirm("Play Again?")) {
+        startGame();
+    } else {
+        alert("Thanks for Playing!")
+    }
+
+    return;
 }
