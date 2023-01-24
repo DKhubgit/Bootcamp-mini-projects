2f4663bbc518d1c0ffc824af1e0fe4c0df172bc6
DanielK <Danielkang13@gmail.com>
2023-01-24 11:06:13 -0800

    added results to show at each round, made sure to account for inappopriate letters used by the user

--- RPS-javascript/script.js
+++ RPS-javascript/script.js
@@ -5,6 +5,11 @@ document.getElementById("button").addEventListener("click", function(event) {
     startGame();
 })
 
+let results = {
+    wins: 0,
+    draws: 0,
+    losses: 0
+}
 function startGame() {
     let userChoice = prompt("Rock, Paper, or Scissors? \n r = Rock | p = Paper | s = Scissors")
     //R = 0, P = 1, S = 2
@@ -13,20 +18,28 @@ function startGame() {
 
     if (userChoice == 'r' && compChoice == 'p') {
         alert(`You-${userChoice}, Computer-${compChoice} \n Computer Wins!`)
+        ++results.losses;
     } else if (userChoice == 'p' && compChoice == 's') {
         alert(`You-${userChoice}, Computer-${compChoice} \n Computer Wins!`)
+        ++results.losses;
     } else if (userChoice == 's' && compChoice == 'r') {
         alert(`You-${userChoice}, Computer-${compChoice} \n Computer Wins!`)
+        ++results.losses;
     } else if (userChoice == compChoice) {
         alert(`You-${userChoice}, Computer-${compChoice} \n Draw!`)
+        ++results.draws;
     } else if (userChoice == null) {
         alert ("Thanks for Playing!")
         return;
-    } else {
+    } else if (array.includes(userChoice)){
         alert(`You-${userChoice}, Computer-${compChoice} \n YOU Win!`)
+        ++results.wins;
+    } else {
+        // if none of the if statements executes then the only reason would be that user chose a unappropriate letter
+        alert("Please choose an appropriate letter")
     }
 
-    if (confirm("Play Again?")) {
+    if (confirm(`Play Again? \n Wins: ${results.wins} | Draws: ${results.draws} | Losses: ${results.losses}`)) {
         startGame();
     } else {
         alert("Thanks for Playing!")
