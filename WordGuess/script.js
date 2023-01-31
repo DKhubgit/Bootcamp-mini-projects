b136bb76bb6b64290346173ef867150ca56c1af5
DanielK <Danielkang13@gmail.com>
2023-01-30 18:30:32 -0800

    added game board title and parially done underlines

--- WordGuess/script.js
+++ WordGuess/script.js
@@ -3,7 +3,27 @@
 function startGame() {
     let element = document.getElementById("main-box");
     element.remove();
+    displayGame();
     return;
+}
+
+function displayGame() {
+    let titleEl = document.createElement('h1');
+    titleEl.innerText = "Guess the Word!"
+
+    document.body.appendChild(titleEl);
+
+    let unorderedEl = document.createElement('ul')
+    document.body.appendChild(unorderedEl);
+
+    let listEl;
+
+    for (let i = 0; i < 6; ++i) {
+        listEl = document.createElement('li')
+        listEl.innerText = "_";
+        unorderedEl.appendChild(listEl);
+    }
+    
 }
 
 document.getElementById("start").addEventListener('click', function (event) {
