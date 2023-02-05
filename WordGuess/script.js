c2c73ad85e164932d83dbd0af03d6b4ac4adb7be
DanielK <Danielkang13@gmail.com>
2023-02-05 14:45:08 -0800

    added event listener for any key pressed and checks to see if a letter is in the word

--- WordGuess/script.js
+++ WordGuess/script.js
@@ -1,4 +1,4 @@
-
+const words = ['BELT', 'SWALLOW', 'CONDUCTOR', 'HOOK', 'SUIT', 'KNOWLEDGE', 'FAST', 'FOOD', 'VEHICLE', 'LOBBY', 'ESSENTIAL', 'BUCKET', 'COMMUNICATION', 'ACCESS', 'LONG', 'RELINQUISH', 'CABLE', 'WINNER', 'FADE', 'IDENTITY', 'RESPECTABLE']
 
 function startGame() {
     let element = document.getElementById("main-box");
@@ -6,6 +6,9 @@ function startGame() {
     displayGame();
     return;
 }
+
+let randIndex = Math.floor(Math.random() * words.length);
+let letters = words[randIndex].split('');
 
 function displayGame() {
     let titleEl = document.createElement('h1');
@@ -18,9 +21,10 @@ function displayGame() {
 
     let listEl;
 
-    for (let i = 0; i < 6; ++i) {
+    for (let i = 0; i < letters.length; ++i) {
         listEl = document.createElement('li')
         listEl.innerText = "_";
+        listEl.setAttribute('id', i)
         unorderedEl.appendChild(listEl);
     }
     let box = document.createElement('div');
@@ -32,7 +36,24 @@ function displayGame() {
 
     document.body.appendChild(box);
     box.appendChild(inputBox);
-    
+
+    document.getElementById("letterInput").addEventListener("keydown", CheckLetter)
+
+}
+
+function CheckLetter(event) {
+            let key = event.key.toUpperCase();
+            let found = false;
+            while (!found) {
+                if (letters.includes(key)) {
+                    let space = document.getElementById(`${letters.indexOf(key)}`)
+                    space.innerText = key;
+                    letters.splice(letters.indexOf(key), 1, 'done');
+                } else {
+                    found = true;
+                }
+            }
+            document.getElementById('letterInput').value = "";
 }
 
 document.getElementById("start").addEventListener('click', function (event) {
