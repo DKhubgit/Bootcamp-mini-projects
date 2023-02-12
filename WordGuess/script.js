b65eb79ae0c7d5b691e992b773db138fa0c5c084
DanielK <Danielkang13@gmail.com>
2023-02-12 13:36:51 -0800

    added end display when user wins or loses, removed newGame function and implemented it's functionality into the displayGame function, displays a button to start a new game

--- WordGuess/script.js
+++ WordGuess/script.js
@@ -1,20 +1,29 @@
 const words = ['BELT', 'SWALLOW', 'CONDUCTOR', 'HOOK', 'SUIT', 'KNOWLEDGE', 'FAST', 'FOOD', 'VEHICLE', 'LOBBY', 'ESSENTIAL', 'BUCKET', 'COMMUNICATION', 'ACCESS', 'LONG', 'RELINQUISH', 'CABLE', 'WINNER', 'FADE', 'IDENTITY', 'RESPECTABLE']
 
 function startGame() {
-    let element = document.getElementById("main-box");
-    element.remove();
+    //remove all the elements in the body
+    let body = document.body
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
     displayGame();
     return;
 }
 
-let randIndex = Math.floor(Math.random() * words.length);
-let letters = words[randIndex].split('');
+let randIndex;
+let letters;
 let finishCount = 0;
-let timeLimit = 120; //sec
-
+let timeLimit = 20; //sec
+let scores = {
+    win: 0,
+    lose: 0
+}
+let interval;
 
 function displayGame() {
     setTime();
+    randIndex = Math.floor(Math.random() * words.length);
+    letters = words[randIndex].split('');
     //title
     let titleEl = document.createElement('h1');
     titleEl.innerText = "Guess the Word!"
@@ -77,47 +86,69 @@ function CheckLetter(event) {
             document.getElementById('letterInput').value = "";
             if (finishCount == letters.length) {
                 finishCount = 0;
-                newGame();
+                clearInterval(interval);
+                endDisplay(true);
             }
             return;
 }
 
-function newGame() {
-    randIndex = Math.floor(Math.random() * words.length);
-    letters = words[randIndex].split('');
-
-    let sub = document.getElementById('ulEl');
-    sub.remove();
-
-    let newUnorderEl = document.createElement('ul');
-    newUnorderEl.setAttribute('id', 'ulEl')
-    
-    document.getElementById('letterBox').appendChild(newUnorderEl);
-
-    let newListEl;
-
-    for (let i = 0; i < letters.length; ++i) {
-        newListEl = document.createElement('li')
-        newListEl.innerText = "_";
-        newListEl.setAttribute('id', i)
-        newUnorderEl.appendChild(newListEl);
+function endDisplay(userWin) {
+    //remove all the elements in the body
+    let body = document.body
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
     }
 
-    document.getElementById("letterInput").addEventListener("keydown", CheckLetter)
+    //replace with new display
+    let finishHeader = document.createElement('h1');
+    finishHeader.innerText =  "GAME END"
+    body.appendChild(finishHeader);
 
+    let winOrLoseHeader = document.createElement("h1");
+
+    if (userWin) {
+        winOrLoseHeader.innerText = "You Win!" 
+        body.appendChild(winOrLoseHeader);
+        scores.win++;
+    } else {
+        winOrLoseHeader.innerText = "You Lose..."
+        body.appendChild(winOrLoseHeader);
+        scores.lose++;
+    }
+
+    //show the word
+    let guessedWord = document.createElement('h1');
+    guessedWord.innerText = "The word was: " + words[randIndex];
+    body.appendChild(guessedWord);
+
+    //show score
+    let scoreBoard = document.createElement('h1');
+    scoreBoard.innerText = `Win: ${scores.win} | Loss: ${scores.lose}`;
+    body.appendChild(scoreBoard);
+
+    //new game Button
+    let buttonBox = document.createElement('div')
+    buttonBox.setAttribute('id', 'main-box')
+    let newGameBtn = document.createElement('button');
+    newGameBtn.setAttribute('id', 'newGame')
+    newGameBtn.innerText = "New Game";
+    buttonBox.appendChild(newGameBtn)
+    body.appendChild(buttonBox);
+
+    document.getElementById('newGame').addEventListener('click', startGame);
 }
 
 function setTime() {
-    let interval = setInterval(function () {
-        timeLimit--
-        document.getElementById('timeEl').innerHTML = timeLimit + 'Sec';
+    interval = setInterval(function () {
+    timeLimit--
+    document.getElementById('timeEl').innerText = timeLimit + ' Sec';
 
-        if (timeLimit == 0) {
-            //quit game
-            clearInterval(interval);
-        }
-
-    }, 1000)
+    if (timeLimit == 0) {
+        //quit game
+        clearInterval(interval);
+        endDisplay(false);
+    }
+}, 1000)
 }
 
 document.getElementById("start").addEventListener('click', function (event) {
