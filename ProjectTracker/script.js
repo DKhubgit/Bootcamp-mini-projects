64e6a3ce2ec67fdcb84e89ef7f0fd719736ecbcd
DanielK <Danielkang13@gmail.com>
2023-03-05 13:31:22 -0800

    added bootstraps card for the instructions and modal button, added forms elements to modal to enter project information, added Jquery UI API for datepicker.

--- ProjectTracker/script.js
+++ ProjectTracker/script.js
@@ -1,13 +1,17 @@
 
+$("#datePicker").datepicker();
+
 var time = moment();
 
+$("#timeSlot").text(time.format("MMM DD YYYY, hh:mm:ss A"));
+
 function showTime() {
-    
+
     var interval = setInterval(function (){
         time.add(1,"s");
         $("#timeSlot").text(time.format("MMM DD YYYY, hh:mm:ss A"));
     }, 1000);
-    
+
 }
 
 showTime();
