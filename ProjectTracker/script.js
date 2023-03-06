d01e44f6b5460e68840c3128e16dc2bdce7d5f25
DanielK <Danielkang13@gmail.com>
2023-03-06 11:02:18 -0800

    User can submit  the modal form and print it to the table in the page, table is responsive, initial table headers are on the page

--- ProjectTracker/script.js
+++ ProjectTracker/script.js
@@ -15,3 +15,26 @@ function showTime() {
 }
 
 showTime();
+
+
+$("#formSubmit").on("click", getData);
+
+function getData() {
+    let data = [];
+    $("input, select").each(function() {
+        data.push($(this).val());
+    })
+    console.log(data)
+    addProject(data);
+}
+
+function addProject(data) {
+    let tableRows = ["<tr>"];
+    $.each(data,function(index, value) {
+        tableRows.push("<td>" + value + "</td>");
+    })
+    tableRows.push("</tr>");
+    $("tbody").append(tableRows.join(''));
+}
+//TODO: Calculate time difference from current to due date
+//TODO: Calculate total earned, 8 hours x time difference
